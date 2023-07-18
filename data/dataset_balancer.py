f55bdc0a5323cb7f24543362b38a1fc3ae27bb8a
Jordi Carrera Ventura <jordi.carrera.ventura@gmail.com>
2023-07-18 15:18:26 +0200

    Implement dataset deduplication along X axis

--- data/dataset_balancer.py
+++ data/dataset_balancer.py
@@ -1,5 +1,5 @@
 import random
-from collections import Counter
+from collections import Counter, defaultdict
 from doctest import testmod
 
 from sklearn.datasets import make_classification
@@ -12,6 +12,30 @@ class DatasetBalancer:
     def __init__(self):
         return
 
+
+    def __make_disambiguation(self, X, Y, margin=0.3):
+        preferences = defaultdict(Counter)
+        for x, y in zip(X, Y):
+            key = x if isinstance(x, str) else tuple(x)
+            preferences[key][y] += 1
+
+        disambiguations = dict([])
+        for x, ys in preferences.items():
+            y_dist = ys.most_common()
+            best_y, best_freq = y_dist[0]
+            if len(y_dist) == 1:
+                disambiguations[x] = best_y
+                continue
+
+            next_freq, next_y = y_dist[1]
+            if next_freq / best_freq <= margin:
+                disambiguations[x] = best_y
+            else:
+                disambiguations[x] = None
+
+        return disambiguations
+
+
     def transform(self, X, Y):
         """
         Parameters
@@ -22,10 +46,13 @@ class DatasetBalancer:
         are met:
         1. All majority classes in Y are subsampled down to the number of ins-
            tances of the minority class.
-        2. Stratified sampling for all Y based on the clusters in their asso-
+        2. For instances `x \in X`  associated with multiple `y \in Y`, keep
+           only the most frequent `y` if it's significantly more frequently
+           than the rest; discard the sample otherwise.
+        3. Stratified sampling for all Y based on the clusters in their asso-
            ciated `Y`. Elbow method for selecting the number of clusters in
            each Y.
-        3. Duplicate removal within each class Y, implemented as diversity sam-
+        4. Duplicate removal within each class Y, implemented as diversity sam-
            pling within each cluster: for each class, an instance is collected
            for each cluster in each iteration. During that one iteration, the
            order of candidates for sampling in that cluster is weighted by di-
@@ -73,16 +100,37 @@ class DatasetBalancer:
         >>> assert len(set([Y.count(y) for y in set(Y)])) == 1
 
 
+        3. Deduplication
+
+        >>> db = DatasetBalancer()
+        >>> XY = [
+        ...   ([y for _ in range(5)], y)
+        ...   for y in range(3)
+        ...   for _ in range(3)
+        ... ]
+        >>> XY += [([1 for _ in range(5)], 2) for _ in range(2)]
+        >>> XY += [([3 for _ in range(5)], 2) for _ in range(2)]
+        >>> XY += [([3 for _ in range(5)], 1) for _ in range(1)]
+        >>> X_, Y_ = list(zip(*XY))
+        >>> X, Y = db.transform(X_, Y_)
+        >>> assert 1 not in set(Y)
+        >>> assert [Y_.count(y) == Y.count(y) for y in set(Y)]
+
+
         """
         Y_dist = Counter(Y)
         XY = list(zip(X, Y))
+        disambiguations = self.__make_disambiguation(X, Y)
+        random.seed(181)
         random.shuffle(XY)
         n_samples = min(Y_dist.values())
         Y_sampled = Counter()
         XY_ = []
+        #print(disambiguations)
         for x, y in XY:
-            #print(XY_, Y_sampled)
-            if Y_sampled[y] < n_samples:
+            #print(Y_sampled)
+            if Y_sampled[y] < n_samples \
+            and disambiguations[x if isinstance(x, str) else tuple(x)] == y:
                 XY_.append((x, y))
                 Y_sampled[y] += 1
             if sum(Y_sampled.values()) / len(Y_dist) >= n_samples:
@@ -94,31 +142,94 @@ class DatasetBalancer:
 
 
 
-class Resampler:
-
-    def __init__(self):
-        return
-
-    def transform(self, X, Y):
-        print('Original dataset shape %s' % Counter(Y))
-
-        cc = ClusterCentroids(
-            estimator=MiniBatchKMeans(n_init=1, random_state=0), random_state=42
-        )
-
-        X_res, Y_res = cc.fit_resample(X, Y)
-
-        print('Resampled dataset shape %s' % Counter(Y_res))
-
-        return X_res, Y_res
-
-
+# class Resampler:
+#
+#     def __init__(self):
+#         return
+#
+#     def transform(self, X, Y):
+#         print('Original dataset shape %s' % Counter(Y))
+#
+#         cc = ClusterCentroids(
+#             estimator=MiniBatchKMeans(n_init=1, random_state=0), random_state=42
+#         )
+#
+#         X_res, Y_res = cc.fit_resample(X, Y)
+#
+#         print('Resampled dataset shape %s' % Counter(Y_res))
+#
+#         return X_res, Y_res
+#
+#
 
 
 if __name__ == '__main__':
 
     testmod()
 
+
+#     db = DatasetBalancer()
+#
+#     # Balanced
+#     XY = [
+#         ([y for _ in range(5)], y)
+#         for y in range(3)
+#         for _ in range(3)
+#     ]
+#     X_, Y_ = list(zip(*XY))
+#     X, Y = db.transform(X_, Y_)
+#     for x, y in zip(X, Y):
+#         print(x, y)
+# #     assert set(Y) == set(Y_)
+# #     assert [Y_.count(y) == Y.count(y) for y in set(Y)]
+#
+#
+#
+#     # Unbalanced
+#     XY = [
+#         ([y for _ in range(5)], y)
+#         for y in range(3)
+#         for _ in range(y + 1)
+#     ]
+#     X_, Y_ = list(zip(*XY))
+#     X, Y = db.transform(X_, Y_)
+#     for x, y in zip(X, Y):
+#         print(x, y)
+#     print(set([Y_.count(y) for y in set(Y)]))
+#     print(len(set([Y_.count(y) for y in set(Y)])))
+
+
+
+    # Duplicate X
+
+    db = DatasetBalancer()
+
+    # Balanced
+    XY = [
+        ([y for _ in range(5)], y)
+        for y in range(3)
+        for _ in range(3)
+    ]
+    XY += [([1 for _ in range(5)], 2) for _ in range(2)]
+    XY += [([3 for _ in range(5)], 2) for _ in range(2)]
+    XY += [([3 for _ in range(5)], 1) for _ in range(1)]
+#     for x in XY:
+#         print(x)
+
+
+    X_, Y_ = list(zip(*XY))
+    X, Y = db.transform(X_, Y_)
+#     for x, y in zip(X, Y):
+#         print(x, y)
+    assert 1 not in set(Y)
+    assert [Y_.count(y) == Y.count(y) for y in set(Y)]
+
+
+
+
+
+
+    """
     rsmplr = Resampler()
 
     # Balanced
@@ -150,37 +261,8 @@ if __name__ == '__main__':
         print(x, y)
     print(set([Y_.count(y) for y in set(Y)]))
     print(len(set([Y_.count(y) for y in set(Y)])))
-
-
-
     """
-    db = DatasetBalancer()
-
-    # Balanced
-    XY = [
-        ([y for _ in range(5)], y)
-        for y in range(3)
-        for _ in range(3)
-    ]
-    X_, Y_ = list(zip(*XY))
-    X, Y = db.transform(X_, Y_)
-    assert set(Y) == set(Y_)
-    assert [Y_.count(y) == Y.count(y) for y in set(Y)]
 
 
-
-    # Unbalanced
-    XY = [
-        ([y for _ in range(5)], y)
-        for y in range(3)
-        for _ in range(y + 1)
-    ]
-    X_, Y_ = list(zip(*XY))
-    X, Y = db.transform(X_, Y_)
-    for x, y in zip(X, Y):
-        print(x, y)
-    print(set([Y_.count(y) for y in set(Y)]))
-    print(len(set([Y_.count(y) for y in set(Y)])))
-    """
 
 
