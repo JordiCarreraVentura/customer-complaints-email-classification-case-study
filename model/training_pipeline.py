bda3cb7c55eedaf749e62b9f099ea826bdec2ebf
Jordi Carrera Ventura <jordi.carrera.ventura@gmail.com>
2023-07-19 18:36:45 +0200

    Refactor pipeline for faster experimentation

--- model/training_pipeline.py
+++ model/training_pipeline.py
@@ -8,6 +8,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import precision_score, recall_score
 from sklearn.model_selection import KFold
 from sklearn.pipeline import Pipeline
@@ -27,6 +28,84 @@ from utils import get_credential
 
 
 
+vectorizers = {
+
+    'unigram_vectorizer': TfidfVectorizer(
+        ngram_range = (1, 1),
+        max_df = 1.0,
+        min_df = 1,
+        norm = 'l2',
+        analyzer = 'word'
+    ),
+
+    'unigram_vectorizer_noidf': TfidfVectorizer(
+        ngram_range = (1, 1),
+        max_df = 1.0,
+        min_df = 1,
+        norm = 'l2',
+        analyzer = 'word',
+        use_idf = False
+    ),
+
+    'char_vectorizer': TfidfVectorizer(
+        ngram_range = (3, 5),
+        max_df = 0.3,
+        min_df = 5,
+        norm = 'l2',
+        analyzer = 'char'
+    ),
+
+    'char_vectorizer_noidf': TfidfVectorizer(
+        ngram_range = (3, 5),
+        max_df = 1.0,
+        min_df = 1,
+        norm = 'l2',
+        analyzer = 'char'
+    ),
+
+}
+
+
+
+
+selectors = {
+
+    'selector_lr': LogisticRegression(
+        penalty = 'l2',
+        max_iter = 300,
+        solver = 'sag',
+        n_jobs = 4,
+        multi_class = 'ovr'
+    ),
+
+    'selector_svm': LinearSVC(dual = False)
+
+}
+
+reducer_params = {
+    'reducer_2000': {'prefit': False, 'max_features': 2000},
+    'reducer_3000': {'prefit': False, 'max_features': 3000},
+    'reducer_5000': {'prefit': False, 'max_features': 5000},
+}
+
+classifiers = {
+
+    'classifier_lr': LogisticRegression(
+        penalty = 'l1',
+        max_iter = 100,
+        solver = 'saga',
+        n_jobs = 4,
+        multi_class = 'multinomial'
+    ),
+
+    'classifier_svm': LinearSVC(dual = False),
+
+    'classifier_nb': MultinomialNB()
+
+}
+
+
+
 if __name__ == '__main__':
 
     dataset = pd.read_csv('data/dataset.dedup.resampled.csv')
@@ -41,81 +120,39 @@ if __name__ == '__main__':
     tracking_server = MlflowTrackingServer(host=get_credential(host_file))
     tracking_server.createif_experiment(HOME)
 
-
-    unigram_vectorizer__params = {
-        'ngram_range': (1, 1),
-        'max_df': 1.0,
-        'min_df': 1,
-        'norm': 'l2',
-        'analyzer': 'word'
-    }
-    char_vectorizer__params = {
-        'ngram_range': (3, 5),
-        'max_df': 0.3,
-        'min_df': 5,
-        'norm': 'l2',
-        'analyzer': 'char'
-    }
-#     selection_lr__params = {
-#         'penalty': 'l2',
-#         'max_iter': 300,
-#         'solver': 'sag',
-#         'n_jobs': 4,
-#         'multi_class': 'ovr',
-#     }
-    selection_svm__params = {
-        'dual': False
-    }
-    lowdim_reducer__params = {
-        'prefit': False,
-        'max_features': 2000
-    }
-#     classifier_params = {
-#         'penalty': 'l1',
-#         'max_iter': 100,
-#         'solver': 'saga',
-#         'n_jobs': 4,
-#         'multi_class': 'multinomial'
-#     }
-    classifier_params = {
-        'dual': False
-    }
-
-    unigram_vectorizer = TfidfVectorizer(**unigram_vectorizer__params)
-    char_vectorizer = TfidfVectorizer(**char_vectorizer__params)
-    #selector = LogisticRegression(**selection_lr__params)
-    selector = LinearSVC(**selection_svm__params)
-    lowdim_reducer = SelectFromModel(selector, **lowdim_reducer__params)
-    #classifier = LogisticRegression(**classifier_params)
-    classifier = LinearSVC(**classifier_params)
-
-
-    vectorizer_params = char_vectorizer__params
-    selector_params = selection_svm__params
-    reducer_params = lowdim_reducer__params
-    classifier_params = classifier_params
-    vectorizer = char_vectorizer
-    reducer = lowdim_reducer
-    classifier = classifier
-
+    components = dict([])
     full_params = dict([])
-    for component_name, params in [
-        ('vectorizer', vectorizer_params),
-        ('selector', selector_params),
-        ('reducer', reducer_params),
-        ('classifier', classifier_params),
+    run_name__parts = []
+    for abstract_name, component_name, registry in [
+        ('vectorizer', 'char_vectorizer_noidf', vectorizers),
+        ('reducer', 'reducer_2000', reducer_params),
+        ('selector', 'selector_svm', selectors),
+        #('classifier', 'classifier_nb', classifiers),
+        ('classifier', 'classifier_svm', classifiers),
     ]:
-        full_params.update({
-            f'{component_name}__{key}': val
-            for key, val in params.items()
-        })
+        full_params[f'{abstract_name}'] = component_name
+        run_name__parts.append(component_name)
+        record = registry[component_name]
+
+        if not isinstance(record, dict):
+            full_params.update({
+                f'{abstract_name}__{key}': val
+                for key, val in record.get_params().items()
+            })
+        else:
+            full_params.update({
+                f'{abstract_name}__{key}': val
+                for key, val in record.items()
+            })
+        components[abstract_name] = registry[component_name]
+    run_name = '.'.join(run_name__parts)
+
 
     kf = KFold(n_splits=5, shuffle=True)
     for _n_fold, (train_idx, test_idx) in enumerate(kf.split(X)):
 
         try:
             n_fold = _n_fold + 1
-            run_name = 'char-svm-svm'
             mlflow.start_run(run_name=f'{run_name}/fold={n_fold}')
         except Exception:
             pass
@@ -129,10 +166,12 @@ if __name__ == '__main__':
         Y_test = [Y[idx] for idx in test_idx]
 
         pipe = Pipeline([
-            #('vectorizer', unigram_vectorizer),
-            ('vectorizer', vectorizer),
-            ('reducer', reducer),
-            ('classifier', classifier)
+            ('vectorizer', components['vectorizer']),
+            ('reducer', SelectFromModel(
+                components['selector'],
+                **components['reducer']
+            )),
+            ('classifier', components['classifier'])
         ])
 
         pipe.fit(X_train, Y_train)
@@ -143,38 +182,6 @@ if __name__ == '__main__':
         mlflow.log_metric('accuracy', score)
         mlflow.end_run()
 
+        mlflow.sklearn.save_model(pipe, f"artifacts/{run_name}/{n_fold}")
 
-
-"""
-    # LR3: 81.5% micro-averaged precision and recall, lowest variance
-
-    config['vectorizer']: {
-        'ngram_range': (1, 1),
-        'max_df': 1.0,
-        'min_df': 1,
-        'norm': 'l2',
-        'analyzer': 'word'
-    }
-    config['feature_selector'] = {
-        'penalty': 'l2',
-        'max_iter': 300,
-        'solver': 'sag',
-        'n_jobs': 4,
-        'multi_class': 'ovr',
-    }
-    config['feature_selection'] = {
-        'max_features': 3000
-    }
-    config['classifier']: {
-        'penalty': 'l1',                # slightly better than L2
-        #'penalty': 'l2',
-        'max_iter': 100,
-        'solver': 'saga',
-        'n_jobs': 4,
-        #'multi_class': 'ovr',
-        'multi_class': 'multinomial',   # better than 'ovr'
-        'model': LogisticRegression,
-    }
-
-"""
 
